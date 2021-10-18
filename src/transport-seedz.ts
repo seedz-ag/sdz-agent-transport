47dc6e40084d481fb4a1d393624333f1d9a83be0
Diogo <diogo.nunes>
2021-10-18 13:46:43 -0300

    new api created

---
+++ src/transport-seedz.ts
@@ -0,0 +1,50 @@
+import { AxiosResponse, Method } from "axios";
+import Transport from "./transport";
+
+export default class TransportSeedz extends Transport {
+  constructor(credentials: any) {
+    super("");
+  }
+
+  private async request<T>(
+    method: Method = "GET",
+    url: string,
+    data: any,
+    needsToken = false
+  ): Promise<T> {
+    return this.agent.request({
+      data,
+      method,
+      url,
+    });
+  }
+
+  async send(entity: string, body: any): Promise<AxiosResponse | void> {
+    try {
+      return this.request(
+        "POST",
+        "/batch",
+        {
+          body,
+          id: "",
+          method: "POST",
+          headers: {
+            client: "",
+            guid: ""
+          },
+          uri: this.getURIMap(entity),
+        },
+        true
+      );
+    } catch (exception: unknown) {
+      this.onError(exception);
+    }
+  }
+
+  private getURIMap(entity: string): string {
+    const map: { [key: string]: string } = {
+      cliente: 'client'
+    };
+    return map[entity];
+  }
+}

f12107440cf94450d0d6793bd17ccbdc7e21baa2
Diogo Nunes <diogo.nunes>
2022-01-20 12:45:20 -0300

    open-id authentication

--- dist/transport-seedz.js
+++ dist/transport-seedz.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const transport_1 = __importDefault(require("./transport"));
 class TransportSeedz extends transport_1.default {
-    constructor(apiUrl, credentials) {
+    constructor(issuerUrl, apiUrl, credentials) {
         super(apiUrl);
         this.uriMap = {};
         this.setCredentials(credentials);
+        this.setIssuerURL(issuerUrl);
     }
     // GETTERS AND SETTERS
     getCredentials() {
@@ -18,6 +19,19 @@ class TransportSeedz extends transport_1.default {
         this.credentials = credentials;
         return this;
     }
+    getIssuerURL() {
+        return this.issuerUrl;
+    }
+    setIssuerURL(issuerUrl) {
+        this.issuerUrl = issuerUrl;
+        return this;
+    }
+    getOpenIdHeaders() {
+        return {
+            Authorization: `Bearer ${this.token}`,
+            ClientId: this.getCredentials().client_id,
+        };
+    }
     getUriMap() {
         return this.uriMap;
     }
@@ -25,19 +39,37 @@ class TransportSeedz extends transport_1.default {
         this.uriMap = uriMap;
         return this;
     }
-    async request(method = "GET", url, data, needsAuthentication = false) {
+    // METHODS
+    async authenticate() {
+        try {
+            const { data } = await this.agent.request({
+                url: `${this.getIssuerURL()}/token`,
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                data: `client_id=${encodeURIComponent(this.getCredentials().client_id)}&client_secret=${encodeURIComponent(this.getCredentials().client_secret)}&grant_type=client_credentials`,
+                method: "POST",
+            });
+            this.token = data.access_token;
+        }
+        catch (e) {
+            throw new Error("Authentication failed");
+        }
+    }
+    async request(method = "GET", url, data) {
         return this.agent.request({
             data,
             headers: {
-                ...(needsAuthentication ? this.getCredentials() : {})
+                ...this.getOpenIdHeaders(),
             },
+            maxBodyLength: 1000000000,
+            maxContentLength: 100000000,
             method,
             url,
         });
     }
     async send(entity, body) {
         try {
-            return this.request("POST", this.uriMap[entity], body, true);
+            !this.token && (await this.authenticate());
+            return this.request("POST", this.uriMap[entity], body);
         }
         catch (exception) {
             this.onError(exception);
